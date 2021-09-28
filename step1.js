a9e56b3c8c6c664c5a69e18aa419d5f8aa1668f5
Lizzy Ahler <ahlerliz@gmail.com>
2021-09-28 12:34:20 -0700

    adding step1

---
+++ step1.js
@@ -0,0 +1,25 @@
+// module.exports = {
+//     cat: cat
+//   };
+
+const fsP = require("fs/promises");
+
+const argv = process.argv;
+
+async function cat(path) {
+  try {
+    let contents = await fsP.readFile(path, "utf8");
+    console.log("file contents:", contents);
+  } catch (err) {
+    console.log(`Error: ENOENT: no such file or directory, open ${argv[2]}`)
+    process.exit(1);
+  }
+}
+// end
+//console.log(process.argv);
+cat(argv[2]);
+//cat(argv[1]);
+
+
+
+// process.argv
